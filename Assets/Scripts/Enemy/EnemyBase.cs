b47e1faa0a70a8e10728c0195cd98abe6fe2341c
Llwnr <noregrets.llwnr@gmail.com>
2023-04-02 11:41:47 +0545

    test

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -7,7 +7,7 @@ public class EnemyBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("enemy");
     }
 
     // Update is called once per frame
